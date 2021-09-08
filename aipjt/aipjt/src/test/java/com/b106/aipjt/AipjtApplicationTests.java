d2b7ee39d35f6037a77750d96a8b7254f0f64025
junnnyyy <junnnyyy@kakao.com>
2021-09-08 22:42:31 +0900

    #S05P21B106-302 feat: Spring 프로젝트 생성

---
+++ aipjt/aipjt/src/test/java/com/b106/aipjt/AipjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b106.aipjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
