aa7d79d61713d2cd9c80ba6c13f3e4251a31a6d9
jnheo1216 <hjeionyng94@gmail.com>
2021-09-09 14:18:43 +0900

    S05P21B106-306 feat: 프론트 스켈레톤 프로젝트 생성

---
+++ front/src/components/game/DrawingTool.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>드로잉툴</div>
+</template>
+
+<script>
+export default {
+  name: 'DrawingTool'
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/src/components/game/GameResult.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>게임 결과 출력</div>
+</template>
+
+<script>
+export default {
+  name: 'GameResult'
+}
+</script>
+
+<style>
+
+</style>
