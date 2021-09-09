f046448282add0fde5820a86352d9d1d8496a7a6
한건 <ofliberty@naver.com>
2021-09-09 15:41:15 +0900

    #S05P21B106-305 feat: 프론트 스켈레톤 프로젝트 생성(router)

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Lobby from '../views/Lobby.vue'
+import GameInit from '../views/GameInit.vue'
+import GamePlay from '../views/GamePlay.vue'
+import GameResult from '../views/GameResult.vue'
 
 const routes = [
   {
@@ -8,13 +12,25 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/Lobby',
+    name: 'Lobby',
+    component: Lobby
+  },
+  {
+    path: '/GameInit',
+    name: 'GameInit',
+    component: GameInit
+  },
+  {
+    path: '/GamePlay',
+    name: 'GamePlay',
+    component: GamePlay
+  },
+  {
+    path: '/GameResult',
+    name: 'GameResult',
+    component: GameResult
+  },
 ]
 
 const router = createRouter({
