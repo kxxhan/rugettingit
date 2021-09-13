234830d2946d93257d39aea1ad1206a1f3d3bba9
이정준 <junnnyyy@kakao.com>
2021-09-13 14:22:18 +0900

    #S05P21B106-365 feat: test controller 생성

---
+++ spring_backend/aipjt/src/main/java/com/b106/aipjt/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.b106.aipjt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String hello() {
+        return "Hello, test success";
+    }
+}
