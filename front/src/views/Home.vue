468598b3c19da382928305c382534e0b2d00ae88
한건 <ofliberty@naver.com>
2021-09-15 00:24:29 +0900

    Canvas

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -3,11 +3,16 @@
     <img alt="Vue logo" src="../assets/logo.png">
   </div>
   <div>서비스메인화면</div>
+  <Canvas />
 </template>
 
 <script>
+import Canvas from '../components/game/Canvas.vue'
 
 export default {
+  components: {
+    Canvas
+  },
   name: 'Home',
 }
 </script>
