526db9b6ce97b22179e2e25b7e0438c6db669b5f
wns312 <wns312@gmail.com>
2021-09-16 01:20:48 +0900

    #S05P21B106-440 feat: Spring 예외처리 구현

---
+++ spring_backend/aipjt/src/main/java/com/b106/aipjt/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.b106.aipjt.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    NOT_NULL("001", "널"),
+    RUN_TIME("002", "런타임에러"),
+    NOT_FOUND("999", "기타에러");
+
+    private String code;
+    private String description;
+}
