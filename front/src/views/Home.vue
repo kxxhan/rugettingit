96cc14f9bf527494d65fd1ed39ebf08cae9f40b1
한건 <ofliberty@naver.com>
2021-09-16 15:51:48 +0900

    #S05P21B106-406 feat:Home 화면 구현

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -4,22 +4,19 @@
       <img alt="Vue logo" src="../assets/ccc.png" height="200" width="200">
     </header>
     <h1>서 비 스  명</h1>
-    <div>
+    <div class="mainService">
       <Avatar />
-      <MainButton />
     </div>
   </body>
 </template>
 
 <script>
 import Avatar from '../components/setting/Avatar.vue'
-import MainButton from '../components/buttons/MainButton.vue'
 
 export default {
   name: 'Home',
   components: {
     Avatar,
-    MainButton,
   },
 }
 </script>
@@ -31,6 +28,17 @@ body {
   display: flex;
   flex-direction: column;
   align-items: center;
+  /* width: 100%; */
+  padding-top: 50px;
+  padding: 50px 0px;
+}
+.mainService {
+  background-color: #f6f9fc;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  /* width: 100%; */
   padding-top: 50px;
   padding: 50px 0px;
 }
