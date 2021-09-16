17e67ae324b39caa826036bb93a5746126dd071b
jnheo1216 <hjeionyng94@gmail.com>
2021-09-16 17:26:31 +0900

    S05P21B106-448 feat: lobby 페이지 틀 잡기

--- front/src/components/buttons/LobbyButton.vue
+++ front/src/components/buttons/LobbyButton.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>로비에서 게임시작이랑 링크생성</div>
+  <div>
+    <div>로비에서 게임시작이랑 링크생성</div>
+    <button @click="startGame()">게임시작</button>
+    <button @click="generateLink()">초대링크</button>
+    <div>{{ inviteLink }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'LobbyButton'
+  name: 'LobbyButton',
+  data: function() {
+    return {
+      inviteLink: '',
+    }
+  },
+  methods :{
+    generateLink: function () {
+      this.inviteLink = document.location.href
+    },
+    startGame: function () {
+    },
+  }
 }
 </script>
 
--- front/src/views/Lobby.vue
+++ front/src/views/Lobby.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-    로비
+    <div>로비</div>
+    ----------------------------------
+    <UserList/>
+    ----------------------------------
+    <GameSetting/>
+    ----------------------------------
+    <LobbyButton/>
   </div>
 </template>
 
 <script>
+import LobbyButton from '@/components/buttons/LobbyButton.vue'
+import GameSetting from '@/components/setting/GameSetting.vue'
+import UserList from '@/components/user/UserList.vue'
+
 export default {
-  name: 'Lobby'
+  mounted: {
+    // 입장하는순간 소켓 걸어줘야돼
+  },
+  components: {
+    GameSetting,
+    LobbyButton,
+    UserList
+  },
+  name: 'Lobby',
 }
 </script>
 
