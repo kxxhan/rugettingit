f20276a03fe5e58ec6bbb0f052eccfa5ae4e32f2
wns312 <wns312@gmail.com>
2021-09-20 18:22:06 +0900

    #S05P21B106-457 feat: 유저 생성 & 유저 조회 로직 구현

--- back_spring/aipjt/src/main/java/com/b106/aipjt/controller/RoomController.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/controller/RoomController.java
@@ -31,15 +31,15 @@ public class RoomController {
 
     // 방 생성 : 현재 멤버를 미리 생성하는 메소드를 만들지, 멤버를 동시에 만들지 고민이 많은 상태
     // superUser의 id 정보를 노출하지 않는 것이 좋을 것 같은데, 어떻게 해야할까?? 엔티티 수정해야될지도
-    @PostMapping("")
-    public ResponseDto<RoomResponseDto> createRoom(@Valid @RequestBody RoomCreateRequestDto roomCreateDto) throws Exception{
-        Optional<Room> result = roomRedisService.createRoom(roomCreateDto.getAvatar(), roomCreateDto.getNickname());
-        if (result.isPresent()) {
-            Room room = result.get();
-            return new ResponseDto<>(HttpStatus.CREATED.value(), "방 생성 성공", new RoomResponseDto(room.getId(), room.getMemberList().get(0)));
-        }
-        throw new Exception("방 생성 실패");
-    }
+//    @PostMapping("")
+//    public ResponseDto<RoomResponseDto> createRoom(@Valid @RequestBody RoomCreateRequestDto roomCreateDto) throws Exception{
+//        Optional<Room> result = roomRedisService.createRoom(roomCreateDto.getAvatar(), roomCreateDto.getNickname());
+//        if (result.isPresent()) {
+//            Room room = result.get();
+//            return new ResponseDto<>(HttpStatus.CREATED.value(), "방 생성 성공", new RoomResponseDto(room.getId(), room.getMemberList().get(0)));
+//        }
+//        throw new Exception("방 생성 실패");
+//    }
 
 
 }
--- back_spring/aipjt/src/main/java/com/b106/aipjt/exception/ExceptionController.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/exception/ExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,9 +24,9 @@ public class ExceptionController {
     public ResponseEntity<ErrorResponseDto> handleException(Exception e) {
         e.printStackTrace();
         if (e.getClass()==RuntimeException.class){
-            return new ResponseEntity<ErrorResponseDto>(new ErrorResponseDto(ErrorCode.RUN_TIME.getCode(), ErrorCode.RUN_TIME.getDescription()), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<ErrorResponseDto>(new ErrorResponseDto(ErrorCode.RUN_TIME.getCode(), e.getMessage()), HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<ErrorResponseDto>(new ErrorResponseDto(ErrorCode.NOT_NULL.getCode(), ErrorCode.NOT_NULL.getDescription()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<ErrorResponseDto>(new ErrorResponseDto(ErrorCode.NOT_NULL.getCode(), ErrorCode.NOT_NULL.getDescription(), e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -36,6 +37,13 @@ public class ExceptionController {
         List<InvalidDataDto> invalidDatas = new ArrayList<>();
         fieldErrors.forEach(f -> {invalidDatas.add(new InvalidDataDto(f.getField(), f.getRejectedValue(), f.getDefaultMessage(), f.getCode()));});
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(ErrorCode.INVALID_DATA.getCode(), ErrorCode.INVALID_DATA.getDescription(), invalidDatas);
-        return new ResponseEntity<ErrorResponseDto>(errorResponseDto, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity(errorResponseDto, HttpStatus.BAD_REQUEST);
+    }
+    // 헤더 정보가 없을 때
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ErrorResponseDto> missingUserHeader(MissingRequestHeaderException e) {
+        e.printStackTrace();
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(ErrorCode.NO_USER_HEADER.getCode(), ErrorCode.NO_USER_HEADER.getDescription());
+        return new ResponseEntity(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 }
