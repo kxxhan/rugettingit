aac68832cb05f57532b1dcee9141b0a3f30b7357
이정준 <junnnyyy@kakao.com>
2021-09-21 00:01:15 +0900

    #S05P21B106-405 feat: 웹 소켓 구현/be

--- back_spring/aipjt/src/main/java/com/b106/aipjt/config/StompWebSocketConfig.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/config/StompWebSocketConfig.java
@@ -19,10 +19,12 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        // 헤더의 데스티네이션이 /sub로 시작하는 메서드로 라우팅 됨
-        config.setApplicationDestinationPrefixes("/app");
+        // 헤더의 데스티네이션이 /pub으로  시작하는 메서드로 메시지가 @MessageMapping으로 메서드 라우팅 됨
+        // 이 메시지를 브로드캐스팅 해줘
+        config.setApplicationDestinationPrefixes("/pub");
         // 구독과 브로드캐스팅에 내장된 심플브로커를 이용
         // 이 해당 브로커로 데스티네이션주소가 시작되는 클라이언트에 메시지를 라우팅
-        config.enableSimpleBroker("/topic");
+        // 나 이 토픽을 구독할래
+        config.enableSimpleBroker("/sub");
     }
 }
