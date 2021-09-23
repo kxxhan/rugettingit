ab1f641cdaecb9a94773a0e1dd0f48d45fdf737c
이정준 <junnnyyy@kakao.com>
2021-09-23 12:04:24 +0900

    #S05P21B106-405 feat: 웹 소켓 구현/be

--- back_spring/aipjt/src/main/java/com/b106/aipjt/controller/SocketChatController.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/controller/SocketChatController.java
@@ -7,26 +7,26 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class StompChatController {
+public class SocketChatController {
 
     private final SimpMessagingTemplate template;
 
     @Autowired
-    public StompChatController(SimpMessagingTemplate template) {
+    public SocketChatController(SimpMessagingTemplate template) {
         this.template = template;
     }
 
-    // prefix로 topic이 설정이 되어있으므로 /pub/chat/enter로 퍼블리쉬 요청이 오는 것
+    // prefix로 pub 설정이 되어있으므로 /pub/chat/enter로 퍼블리쉬 요청이 오는 것
     @MessageMapping(value = "/chat/enter")
     public void enterRoom(ChatMessageDto chatMessageDto) {
         chatMessageDto.setMessage(chatMessageDto.getWriter() + " 두둥 등장!");
-        // /topic/chat/room/{roomId}를 구독하는 애들한테 메시지를 퍼블리쉬
+        // /pub/chat/room/{roomId}를 구독하는 애들한테 메시지를 퍼블리쉬
         template.convertAndSend("/sub/chat/room/" + chatMessageDto.getRoomId(), chatMessageDto);
     }
 
     @MessageMapping(value = "/chat/message")
     public void message(ChatMessageDto chatMessageDto) {
-        // /topic/chat/room/{roomId}를 구독하는 애들한테 메시지를 퍼블리쉬
+        // /pub/chat/room/{roomId}를 구독하는 애들한테 메시지를 퍼블리쉬
         template.convertAndSend("/sub/chat/room/" + chatMessageDto.getRoomId(), chatMessageDto);
     }
 }
--- back_spring/aipjt/src/main/java/com/b106/aipjt/domain/repository/ChatRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.b106.aipjt.domain.repository;
-
-import com.b106.aipjt.domain.redishash.ChatRoom;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ChatRoomRepository extends CrudRepository<ChatRoom, String> {
-}
