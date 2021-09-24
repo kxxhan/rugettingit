4f7399ad4101da199b71553ae3e902d2f14774cf
이정준 <junnnyyy@kakao.com>
2021-09-24 17:36:32 +0900

    #S05P21B106-497 feat: 웹 소켓 통신 테스트/fe

--- front/src/views/Chat.vue
+++ front/src/views/Chat.vue
@@ -55,7 +55,7 @@ export default {
       }
     },    
     connect() {
-      const serverURL = "http://localhost:8080/"
+      const serverURL = "http://localhost:8080/stomp/chat"
       let socket = new SockJS(serverURL);
       this.stompClient = Stomp.over(socket);
       console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
