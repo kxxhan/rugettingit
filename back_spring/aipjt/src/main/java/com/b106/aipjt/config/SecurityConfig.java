1bbd017a6da9490e492d75cae990a74a3d9ff86e
이정준 <junnnyyy@kakao.com>
2021-09-24 18:40:44 +0900

    #S05P21B106-497 feat: spring security 해제/be

--- back_spring/aipjt/src/main/java/com/b106/aipjt/config/SecurityConfig.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/config/SecurityConfig.java
@@ -2,14 +2,15 @@ package com.b106.aipjt.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsUtils;
 
+//@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,20 +18,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-            .cors().and()
             .csrf().disable();
+        httpSecurity.authorizeRequests()
+            .anyRequest().permitAll().and().cors().disable();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
+        CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
+        configuration.setMaxAge(3600L);
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("*", configuration);
+        source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 
