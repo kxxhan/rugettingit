25a60306d795f30d5772ae9e7122112066e3b022
이정준 <junnnyyy@kakao.com>
2021-09-27 10:09:42 +0900

    #S05P21B106-585 feat: CORS 해결/be

--- front/src/views/Chat.vue
+++ front/src/views/Chat.vue
@@ -55,7 +55,7 @@ export default {
       }
     },    
     connect() {
-      const serverURL = "http://localhost:8080/stomp/chat"
+      const serverURL = "http://j5b106.p.ssafy.io:8080/stomp/chat"
       let socket = new SockJS(serverURL);
       this.stompClient = Stomp.over(socket);
       console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
