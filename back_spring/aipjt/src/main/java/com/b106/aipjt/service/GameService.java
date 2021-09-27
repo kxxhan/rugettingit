c39e2693bd7a4a6147ae6086c6451acedebcb0a3
wns312 <wns312@gmail.com>
2021-09-28 00:28:51 +0900

    #S05P21B106-607 feat: 게임 라운드 로직 구현

--- back_spring/aipjt/src/main/java/com/b106/aipjt/service/GameService.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/service/GameService.java
@@ -2,9 +2,11 @@ package com.b106.aipjt.service;
 
 import com.b106.aipjt.domain.redishash.Room;
 import com.b106.aipjt.domain.redishash.Round;
+import com.b106.aipjt.domain.redishash.Skip;
 import com.b106.aipjt.domain.redishash.User;
 import com.b106.aipjt.domain.repository.RoomRedisRepository;
 import com.b106.aipjt.domain.repository.RoundRedisRepository;
+import com.b106.aipjt.domain.repository.SkipRedisRepository;
 import com.b106.aipjt.domain.repository.UserRedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -23,6 +25,8 @@ public class GameService {
     private final UserRedisRepository userRedisRepository;
     private final RoundRedisRepository roundRedisRepository;
     private final RoomRedisRepository roomRedisRepository;
+    private final SkipRedisRepository skipRedisRepository;
+
     private final String ROOM_PREFIX = "room/";
 
 
@@ -53,12 +57,65 @@ public class GameService {
     @Async
     public void roundStart(String userId, String roomId) throws InterruptedException {
         // 우선 방장인지부터 확인한다.
+        Optional<User> userById = userRedisRepository.findById(userId);
+        Optional<Room> roomById = roomRedisRepository.findById(roomId);
+        // 검증 1
+        if (userById.isEmpty()) throw new RuntimeException("유저가 존재하지 않습니다");
+        if (roomById.isEmpty()) throw new RuntimeException("방이 존재하지 않습니다");
+        // 꺼낸다
+        User user = userById.get();
+        Room room = roomById.get();
+        // 검증 2
+        if (!user.getId().equals(room.getSuperUser().getId())) throw new RuntimeException("방장만 라운드를 시작할 수 있습니다");
+        if (!room.isStart()) throw new RuntimeException("이미 게임이 종료되었습니다");
 
-        // 현재 라운드부터 반복문을 돌기 시작한다.
+        while (room.getCurrentRound() < room.getMaxRound()) {
+            // 라운드 시작 : 라운드를 0에서 1로 변경해주고, 라운드 시작인 play를 true로 만들어준다
+            room.setCurrentRound(room.getCurrentRound()+1);
+            room.setPlay(true);
 
-        // 모든 라운드가 종료되었다면 결과를 뿌리고 종료한다
+            // 질문 조회해서 Round에 넣어주기
+
+            // 질문 조회했다고 가정하고, 라운드 객체에 값 세팅 후 저장
+            Round round = roundRedisRepository.save(new Round(null, room.getRoundTime(), "문제", 1L));
+
+            // 라운드를 저장하고 room을 저장해야 id가 null이 아니다
+            room.getRound().add(round);
+            room = roomRedisRepository.save(room);
+
+            // 방을 담아서 라운드 시작 메시지를 보낸다
+            template.convertAndSend(ROOM_PREFIX+room.getId(), "방객체");
+
+            // 라운드 진행시간동안 잔다
+            Thread.sleep(room.getRoundTime()*1000);
+
+            // 라운드 종료 -> 이제부터 사이시간
+            // 스킵 객체 생성
+            Skip skip = skipRedisRepository.save(new Skip());
+            // 사이시간이므로 false 세팅 후 room 갱신을 위해 저장
+            room.setPlay(false);
+            room = roomRedisRepository.save(room);
+            // 방객체와 스킵 객체id를 보내준다
+            template.convertAndSend(ROOM_PREFIX+room.getId(), "방객체와 슬립객체");
+            // 사이시간동안 잔다
+            Thread.sleep(40000);
+
+            // 깨어나면 스킵 객체를 조회해서 스킵했는지 확인
+            Optional<Skip> skipById = skipRedisRepository.findById(skip.getId());
+            if (skipById.isEmpty()) return; // 스킵객체가 비어있으면 새 쓰레드가 생성된 것이므로 쓰레드 종료
+
+            // 스킵 객체가 있으면 다음 라운드 진행을 위한 준비를 한다
+            // 스킵객체를 삭제한다
+            skipRedisRepository.delete(skipById.get()); // 스킵객체가 있으면 라운드 스킵 X -> 스킵 객체 삭제
+
+            // 다시 while문이 돌거나, 라운드가 끝났다면 아래로 이동
+
+        }
+        // 여기까지 왔다면 쓰레드 리턴도 아니고 라운드가 다 종료되고 온 것. 따라서 isStart를 false로 만들어주고
+        // currentRound도 0으로 만들어줘야 한다
+        room.setStart(false);
+        room.setCurrentRound(0);
+        room = roomRedisRepository.save(room);
+        // 라운드 객체 비워주는게 빠짐 -> 자동삭제 되니까 괜찮은 것 같다.
     }
-    // 가장 큰 문제. 질문에 대한 정보는 어디에서 가져오나? DB에서 가져오지. 그럼 랜덤으로 가져오나? ㅇㅇ
-    // 랜덤으로 가져온 질문에 대한 답은 어디에 저장하나?? Round에 저장하기. 어차피 백에서 지워져도 프론트에서 저장하고 있으므로
-    // 하지만 직접적인 url을 가지고 있으면 안된다. 참조할 수 있는 데이터를 가지고 있어야 함.
 }
