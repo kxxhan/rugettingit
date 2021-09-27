5d7158d47c96e94f5ab6840a654963ecc41396a8
이정준 <junnnyyy@kakao.com>
2021-09-27 15:24:11 +0900

    #S05P21B106-594 feat: Socket handshaking/FE

--- back_spring/aipjt/src/main/java/com/b106/aipjt/config/SecurityConfig.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/config/SecurityConfig.java
@@ -42,6 +42,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/stomp/**");
+        web.ignoring().antMatchers("/api/stomp/**");
     }
 }
