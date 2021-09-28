1a26fc53189cd13bb256a73588d70231ce763cee
이정준 <junnnyyy@kakao.com>
2021-09-28 23:19:42 +0900

    #S05P21B106-525 feat: Socket Chat/FE

--- back_spring/aipjt/src/main/java/com/b106/aipjt/controller/SocketController.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/controller/SocketController.java
@@ -1,33 +1,33 @@
 package com.b106.aipjt.controller;
 
-import com.b106.aipjt.domain.dto.ChatMessageDto;
+import com.b106.aipjt.domain.dto.MessageDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class SocketChatController {
+public class SocketController {
 
     private final SimpMessagingTemplate template;
 
     @Autowired
-    public SocketChatController(SimpMessagingTemplate template) {
+    public SocketController(SimpMessagingTemplate template) {
         this.template = template;
     }
 
     // prefix로 pub 설정이 되어있으므로 /pub/chat/enter로 퍼블리쉬 요청이 오는 것
     @MessageMapping(value = "/chat/enter")
-    public void enterRoom(ChatMessageDto chatMessageDto) {
-        System.out.println(chatMessageDto.getRoomId());
-        chatMessageDto.setMessage("두둥 등장!");
+    public void enterRoom(MessageDto messageDto) {
+        System.out.println(messageDto.getRoomId());
+        messageDto.setMessage("두둥 등장!");
         // /pub/chat/room/{roomId}를 구독하는 애들한테 메시지를 퍼블리쉬
-        template.convertAndSend("/sub/chat/room/" + chatMessageDto.getRoomId(), chatMessageDto);
+        template.convertAndSend("/sub/chat/room/" + messageDto.getRoomId(), messageDto);
     }
 
     @MessageMapping(value = "/chat/message")
-    public void message(ChatMessageDto chatMessageDto) {
+    public void message(MessageDto messageDto) {
         // /pub/chat/room/{roomId}를 구독하는 애들한테 메시지를 퍼블리쉬
-        template.convertAndSend("/sub/chat/room/" + chatMessageDto.getRoomId(), chatMessageDto);
+        template.convertAndSend("/sub/chat/room/" + messageDto.getRoomId(), messageDto);
     }
 }
