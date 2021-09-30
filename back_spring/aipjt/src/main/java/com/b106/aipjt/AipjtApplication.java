48bd7827b19eb8dbff9a51a1fdc42cf4cee4e302
wns312 <wns312@gmail.com>
2021-09-30 15:00:06 +0900

    #S05P21B106-620 feat: Spring 개발환경과 배포환경 yml파일 분리2

--- back_spring/aipjt/src/main/java/com/b106/aipjt/AipjtApplication.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/AipjtApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class AipjtApplication {
+    public static final String APPLICATION_LOCATIONS = "spring.config.location=" + "classpath:application.yml,"
+            + "classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(AipjtApplication.class, args);
+        new SpringApplicationBuilder(AipjtApplication.class).properties(APPLICATION_LOCATIONS).run(args);
     }
 }
