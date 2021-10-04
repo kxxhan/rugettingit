b4d8f15befda83a3b2accf98f77c2b15c0b017c3
wns312 <wns312@gmail.com>
2021-10-04 13:28:48 +0900

    fix: statue -> status

--- back_spring/aipjt/src/main/java/com/b106/aipjt/domain/dto/room/RoomResponseDto.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/domain/dto/room/RoomResponseDto.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class RoomResponseDto {
     private String id;
     private String superUser;
-    private String statue;
+    private String status;
     @Builder.Default
     private int currentRound = 0;
     @Builder.Default
@@ -32,7 +32,7 @@ public class RoomResponseDto {
     public static RoomResponseDto toRoom(Room room, List<RoomUserResponseDto> userListDto) {
         return RoomResponseDto.builder().id(room.getId())
             .superUser(room.getSuperUser().getId())
-            .statue(room.getStatus())
+            .status(room.getStatus())
             .currentRound(room.getCurrentRound())
             .maxRound(room.getMaxRound())
             .roundTime(room.getRoundTime())
