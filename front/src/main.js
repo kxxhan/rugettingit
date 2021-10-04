0504bcf6608eef0917b8eeabf7424f2b85f45951
이정준 <junnnyyy@kakao.com>
2021-10-04 20:16:27 +0900

    #S05P21B106-627 feat: 홈 스타일링

--- front/src/main.js
+++ front/src/main.js
@@ -14,7 +14,7 @@ import ScrollPanel from 'primevue/scrollpanel'
 import ColorPicker from 'primevue/colorpicker'
 
 import 'primevue/resources/themes/saga-blue/theme.css'       //theme
-import 'primevue/resources/primevue.min.css'       //core css
+// import 'primevue/resources/primevue.min.css'       //core css
 import 'primeicons/primeicons.css'       //icons
 import 'primeflex/primeflex.css'
 
