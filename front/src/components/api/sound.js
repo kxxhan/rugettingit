3d86bafdf7306368a84e5d7600fe6354d3ae9ae0
jnheo1216 <hjeionyng94@gmail.com>
2021-10-05 21:16:55 +0900

    #S05P21B106-574 feat: sounds in chat, gamestart

--- front/src/components/api/sound.js
+++ front/src/components/api/sound.js
@@ -2,5 +2,13 @@ function soundEffect() {
   var audio = new Audio(require("@/assets/sounds/enter.mp3"))
   audio.play()
 }
+function soundEffectChat() {
+  var audio = new Audio(require("@/assets/sounds/chat.wav"))
+  audio.play()
+}
+function soundEffectGamestart() {
+  var audio = new Audio(require("@/assets/sounds/pingpong.mp3"))
+  audio.play()
+}
 
-export {soundEffect};
+export {soundEffect, soundEffectChat, soundEffectGamestart};
