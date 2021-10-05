4e35fed473c4aaee68f710578859a15fe0e23a48
한건 <ofliberty@naver.com>
2021-10-05 20:26:07 +0900

    #S05P21B106-577 feat:GamePlay 라운드 타이머 종료시 result 반환

--- front/src/components/game/GameResult.vue
+++ front/src/components/game/GameResult.vue
@@ -3,13 +3,27 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'GameResult',
   data: function () {
     return
   },
   mounted: function () {
-
+    let form = new FormData()
+    form.append('file', this.$store.state.file)
+    axios({
+      method: 'post',
+      url: '/question',
+      data: form,
+      header: {
+        'Content-Type': 'multipart/form-data'
+      },
+    }).then((res) => {
+      console.log('이얏호 성공이지롱', res)
+    }).catch((err) => {
+      console.log('ㅠㅠ 실패', err.response)
+    })
   }
 }
 </script>
