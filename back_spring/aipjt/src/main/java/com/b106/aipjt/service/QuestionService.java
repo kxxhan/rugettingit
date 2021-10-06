dc251b80df7489f8e65f359ba7489918e58d420a
엄재식 <jasic0627@gmail.com>
2021-10-06 16:12:35 +0900

    #S05P21B106 fix: caption url

--- back_spring/aipjt/src/main/java/com/b106/aipjt/service/QuestionService.java
+++ back_spring/aipjt/src/main/java/com/b106/aipjt/service/QuestionService.java
@@ -42,7 +42,7 @@ public class QuestionService {
     public CaptResponse imgUrlPost(String imgUrl) {
         URI uri = UriComponentsBuilder
             .fromUriString("http://j5b106.p.ssafy.io:8081")
-            .path("/api/image_caption/kr/")
+            .path("/api/image_caption/")
             .build()
             .toUri();
         System.out.println("@ uri 확인 : "+uri);
